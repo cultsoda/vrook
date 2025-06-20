ae56a2d397e4b0680bf5bf77e96bce7f155141c5
cultsoda <59673576+cultsoda@users.noreply.github.com>
2025-06-20 23:26:29 +0900

    문의하기_Update theme-provider.tsx
    타입 에러 수정

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,11 +1,14 @@
 'use client'
 
 import * as React from 'react'
-import {
-  ThemeProvider as NextThemesProvider,
-  type ThemeProviderProps,
-} from 'next-themes'
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
+
+// 간단하고 안전한 Props 타입 정의
+interface ThemeProviderProps {
+  children: React.ReactNode
+  [key: string]: any // 모든 속성 허용
+}
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return <NextThemesProvider {...props as any}>{children}</NextThemesProvider>
 }
